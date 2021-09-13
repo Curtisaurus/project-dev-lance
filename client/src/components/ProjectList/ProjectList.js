0c4fbdbda43ad66eafbab25571b9d6a2ac3f6e54
Jay Jenks <jamesljenks@gmail.com>
2021-09-13 18:05:11 -0400

    Made temporary changes to the ProjectList file to ensure there is data to work with during development.

--- client/src/components/ProjectList/ProjectList.js
+++ client/src/components/ProjectList/ProjectList.js
@@ -1,19 +1,45 @@
 import React from "react";
 import ProjectItem from "../ProjectItem";
 // import { UPDATE_PROJECTS } from "../../utils/actions";
-import { useQuery } from "@apollo/react-hooks";
-import { QUERY_ALL_PROJECTS } from "../../utils/queries";
+// import { useQuery } from "@apollo/react-hooks";
+// import { QUERY_ALL_PROJECTS } from "../../utils/queries";
 // import { idbPromise } from '../../utils/helpers';
 import spinner from "../../assets/images/spinner.gif";
 
 function ProjectList() {
-  const { loading, data } = useQuery(QUERY_ALL_PROJECTS);
+  const projects = [
+    {
+      name: "Best Project EVERRRRRR",
+      owner: null,
+      description: "Most complex project to solve your real world needs ever",
+      image: "https://gfycat.com/anxiousilliteratebabirusa",
+      reqFunds: "50000",
+      acqFunds: "45000",
+    },
+    {
+      name: "Wurst Project",
+      owner: null,
+      description: "It says hello world barely",
+      image: "https://gfycat.com/courteoushandmadebutterfly",
+      reqFunds: "50000",
+      acqFunds: "45000",
+    },
+    {
+      name: "Ok Project",
+      owner: null,
+      description: "Literally Minesweeper",
+      image: "https://i.imgur.com/4zdNjKf.png",
+      reqFunds: "50000",
+      acqFunds: "45000",
+    },
+  ];
+  // const { loading, data } = useQuery(QUERY_ALL_PROJECTS);
 
-  let projects;
+  // let projects;
 
-  if (!loading) {
-    projects = data.allProjects;
-  }
+  // if (!loading) {
+  //   projects = data.allProjects;
+  // }
 
   return (
     <div className="my-2">
@@ -25,7 +51,7 @@ function ProjectList() {
               key={project._id}
               image={project.image}
               projectName={project.name}
-              ownerName={project.owner.name}
+              ownerName={project.owner}
               description={project.description}
               reqFunds={project.reqFunds}
               acqFunds={project.acqFunds}
@@ -35,7 +61,7 @@ function ProjectList() {
       ) : (
         <h3>No projects yet!</h3>
       )}
-      {loading ? <img src={spinner} alt="loading" /> : null}
+      {/* {loading ? <img src={spinner} alt="loading" /> : null} */}
     </div>
   );
 }
